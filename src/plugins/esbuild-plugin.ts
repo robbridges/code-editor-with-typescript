0d48f69e88765e50dfcf0bf3813ee81bf9b9f3f6
robbridges <robbridges@users.noreply.github.com>
2021-05-18 18:07:10 -0700

    We are able to more or less import specific packages now. We just need to make openly available to every package.

--- src/plugins/esbuild-plugin.ts
+++ src/plugins/esbuild-plugin.ts
@@ -1,31 +1,40 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
+
 //esbuild plugin 
 export const unpkgPathPlugin = () => {
   return {
     //name really only for debug purposes only in the event multiple plugins
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // hijacks or overrides ESBuilds natural process on finding out where a file is stored So far we are using a catch all filter for any file, but this will likely change
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
+        console.log('onResolve', args);
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        } else if (args.path ==='tiny-test-pkg') {
+          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a'}
+        }
+        
       });
- 
+
+      // hi jacks the process of building that file. 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
- 
+        // we are overriding ES build trying to look up the file in the file system.
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              import message from 'tiny-test-pkg';
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          };
+        }
+        const { data } = await axios.get(args.path)
+        return {
+          loader: 'jsx',
+          contents: data
         }
       });
     },
