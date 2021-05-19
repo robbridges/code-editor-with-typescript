cb0d0f2e5bf277a71f55c2e3d01136c0666ba961
robbridges <robbridges@users.noreply.github.com>
2021-05-18 17:42:10 -0700

    Added esbuild plugin

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
+import { unpkgPathPlugin }  from './plugins/esbuild-plugin'
 
 
 const App = () => {
@@ -28,12 +29,16 @@ const App = () => {
       return;
     }
 
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015',
+    const result = await ref.current.build({
+      entryPoints:['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
     });
 
-    setCode(result.code);
+    //console.log(result);
+
+    setCode(result.outputFiles[0].text);
   };
 
   return (
---
+++ src/plugins/esbuild-plugin.ts
@@ -0,0 +1,33 @@
+import * as esbuild from 'esbuild-wasm';
+//esbuild plugin 
+export const unpkgPathPlugin = () => {
+  return {
+    //name really only for debug purposes only in the event multiple plugins
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log('onResole', args);
+        return { path: args.path, namespace: 'a' };
+      });
+ 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args);
+ 
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+              import message from './message';
+              console.log(message);
+            `,
+          };
+        } else {
+          return {
+            loader: 'jsx',
+            contents: 'export default "hi there!"',
+          };
+        }
+      });
+    },
+  };
+};
