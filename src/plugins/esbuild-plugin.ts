5c1ad0597f8e73b471ed5c0fc77afbdefc3d1566
robbridges <robbridges@users.noreply.github.com>
2021-05-22 18:04:58 -0700

    Update esbuild-plugin.ts

--- src/plugins/esbuild-plugin.ts
+++ src/plugins/esbuild-plugin.ts
@@ -8,35 +8,37 @@ const fileCache = localForage.createInstance({
 
 
 
-//esbuild plugin 
+//esbuild plugin hijacks or overrides ESBuilds natural process on finding out where a file is stored we are using multiple filters, essentially only stopping once we find index.js
 export const unpkgPathPlugin = (inputCode: string) => {
   return {
-    //name really only for debug purposes only in the event multiple plugins
+    //handle root entry file of index.js
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      // hijacks or overrides ESBuilds natural process on finding out where a file is stored So far we are using a catch all filter for any file, but this will likely change
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
 
+      build.onResolve({filter: /(^index\.js$)/ }, () => {
+        return {
+          path: 'index.js',
+          namespace: 'a'
+        }
+      });
+
+      // weird Regex below, it essentially is looking if we can find the file path './' We are handling realitive paths in the module
+      build.onResolve({filter: /^\.+\//}, (args: any) => {
         // if the returned file contains a import state './' we need to combine the urls together, otherwise the response will be to another url entirely
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            //combine a new url with the two arguments, and get the href property from the object.
-            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
-          };
-        }
+        return {
+          namespace: 'a',
+          //combine a new url with the two arguments, and get the href property from the object.
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
+        };
+      })
 
+      // handle main file of a module. 
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
         // this only works if the unpkg path leads direct to an export statement
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
         } 
-        
-        
       });
 
       // hi jacks the process of building that file. 
