d9cc16ccbb7fb718d8076c532808fd2b0f1250c5
robbridges <robbridges@users.noreply.github.com>
2021-05-24 10:08:23 -0700

    Update code-editor.tsx

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -7,6 +7,13 @@ const CodeEditor = () => {
   height="500px"
   options={{
     wordWrap: 'on',
+    minimap: { enabled: false },
+    showUnused: false,
+    folding: false,
+    lineNumbersMinChars: 3,
+    fontSize: 16,
+    scrollBeyondLastLine: false,
+    
   }} />;
 };
 
