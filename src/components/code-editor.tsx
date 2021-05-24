cf03e7a23395ae5b85e6ed5f466768e18299f7ba
robbridges <robbridges@users.noreply.github.com>
2021-05-24 10:05:30 -0700

    configuring monaco editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,13 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor height="500px" />;
-}
+  return <MonacoEditor 
+  theme='dark' 
+  language="javascript" 
+  height="500px"
+  options={{
+    wordWrap: 'on',
+  }} />;
+};
 
 export default CodeEditor;
