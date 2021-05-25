9faaa4fafd560823775e2c2824f5b70e1d627075
robbridges <robbridges@users.noreply.github.com>
2021-05-24 20:26:18 -0700

    refactoring more

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-
-import { useState } from 'react';
-
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundler from './bundler/index';
-
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] =useState('Enter Text Here');
-  const onClick = async () => {
-    const output = await bundler(input);
-    setCode(output);
-  };
-  
-  
-
   return (
   <div>
-    <CodeEditor initialValue="const a = 1;"
-    onChange={(value) => setInput(value)} />
-    <div>
-      <button onClick={onClick}>Submit</button>
-    </div>
-    <Preview code={code} />
+    <CodeCell />
+    <CodeCell />
   </div>
   );
 };
