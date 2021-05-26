5f2f86a78a593691834c922eb56be624210e7ab2
robbridges <robbridges@users.noreply.github.com>
2021-05-25 18:23:53 -0700

    Logging error instead of crashing the entire program over it!

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -17,7 +17,7 @@ const html = `
             } catch (err) {
               const root = document.querySelector('#root');
               root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              throw err;
+              console.error(err);
             }
           }, false);
         </script>
