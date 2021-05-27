298a2175b898a969bed0b14d15fd3c1d26833ef8
robbridges <robbridges@users.noreply.github.com>
2021-05-27 13:57:17 -0700

    More updates to redux

--- src/redux-state/reducers/cellReducer.ts
+++ src/redux-state/reducers/cellReducer.ts
@@ -1,6 +1,9 @@
+import produce from 'immer';
+
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell, CellTypes } from '../cell';
+
 
 interface CellState {
   loading:boolean;
@@ -18,25 +21,19 @@ const initalState: CellState = {
   data: {}
 }
 
-const reducer = (
+const reducer = produce((
   state: CellState = initalState, 
   action: Action
-): CellState => {
+): CellState | void => {
     switch (action.type) {
       case ActionType.UPDATE_CELL:
         const { id, content} = action.payload;
-        return {
-          ...state,
-          data:{
-            ...state.data, 
-            [id]: {
-              ...state.data[id], 
-              content,
-          },
-        },
-      }
+        state.data[id].content = content;
+        return;
       case ActionType.DELETE_CELL:
-        return state;
+        delete state.data[action.payload];
+        state.order = state.order.filter(id => id !==action.payload);
+        return;
       case ActionType.MOVE_CELL:
         return state;
       case ActionType.INSERT_CELL_BEFORE:
@@ -44,6 +41,6 @@ const reducer = (
       default:
         return state;      
     } 
-};
+});
 
 export default reducer;
